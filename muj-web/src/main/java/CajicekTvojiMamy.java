1b0dd0033d68f93123021a54c85cc2f6c1100dbb
vojtechvojtech <55787341+vojtechvojtech@users.noreply.github.com>
2020-10-19 10:51:08 +0200

    Create CajicekTvojiMamy.java

---
+++ muj-web/src/main/java/CajicekTvojiMamy.java
@@ -0,0 +1,13 @@
+public class CajicekTvojiMamy implements Jidlo {
+    public int dejCenu() {
+        return 150;
+    }
+
+    public String vyrobTo() {
+        return "Dej uvarit vodu," +
+                "priprav si svuj oblibeny hrnicek," +
+                "kup si caj na https://www.caj.cz/ceylon-ruhuna-fbopfexsp-nilwala~z171-000194.html," +
+                "hod ho do hrnicku s uvarenou vodou a pockej cca 4 minuty," +
+                "potom muzes pridat citron a med a bude to jak od maminky.";
+    }
+}
